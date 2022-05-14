ea0f1dba674ef568bdd7f9219332127184653df2
Aman Singh <amankumarsingh.ae@gmail.com>
2022-05-15 00:20:30 +0530

    Added httm methods/operations: get and post

--- app/main.py
+++ app/main.py
@@ -1,7 +1,43 @@
+from random import randrange
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+import pydantic
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{"title": "title of post 1", "contnent": "content of post 1", "id": 1},
+            {"title": "favourite food", "contnent": "I like Pizza", "id": 2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to my API"}
+
+@app.get("/posts")
+def get_post():
+    return {"data": my_posts}
+
+@app.post("/posts")
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    print(post)
+    return {"post details": post}
